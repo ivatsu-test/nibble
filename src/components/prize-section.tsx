d4042a5179b78983b48345d05d93aab32ed344a5
IvaTsu <ivan.tsud@gmail.com>
2025-02-20 21:19:30 +0100

    feat: active button state of prize section

--- src/components/prize-section.tsx
+++ src/components/prize-section.tsx
@@ -4,23 +4,43 @@
 
 'use client';
 
-import { useMemo, useState } from 'react';
 import clsx from 'clsx';
+import { useMemo, useState } from 'react';
 import Image from 'next/image';
 import { useMediaQuery } from 'usehooks-ts';
 import { Question } from '@/models/question';
-import styles from './prize-section.module.css';
+import useGameStore from '@/store/gameStore';
 import closeIcon from '../../public/close-icon.svg';
 import menuIcon from '../../public/menu-icon.svg';
 import AnswerOptionButton from './buttons/answer-option-button/answer-option-button';
+import styles from './prize-section.module.css';
+
+const answerOptionButtonState = ({ questionId, currentQuestionIndex }: {
+  questionId: number;
+  currentQuestionIndex: number;
+}) => {
+  const currentQuestionId = currentQuestionIndex + 1;
+  if (questionId === currentQuestionId) {
+    return 'active';
+  }
+  if (questionId < currentQuestionId) {
+    return 'inactive';
+  }
+  return 'default';
+};
 
 export default function PrizeSection({ questions }: { questions: Question[] }) {
+  const { currentQuestionIndex } = useGameStore();
   const isDesktop = useMediaQuery('(min-width: 1200px)');
   const [isMenuOpen, setIsMenuOpen] = useState(false);
   const reversedQuestions = useMemo(() => [...questions].reverse(), [questions]);
 
   function handleMenuClick() {
     setIsMenuOpen(!isMenuOpen);
+  }
+
+  function buttonState(questionId: number) {
+    return answerOptionButtonState({ questionId, currentQuestionIndex });
   }
 
   return (
@@ -37,11 +57,14 @@ export default function PrizeSection({ questions }: { questions: Question[] }) {
 
       <div className={clsx(styles['prize-section'], isMenuOpen && styles['prize-section--active'])}>
         <div className={styles['prize-section__items']}>
-          {reversedQuestions.map((question) => (
-            <AnswerOptionButton key={question.id} kind={isDesktop ? 'prize-desktop' : 'prize-mobile'} onClick={() => {}}>
-              <div className={styles['l-question-prize']}>{question.prize}</div>
-            </AnswerOptionButton>
-          ))}
+          {reversedQuestions.map((question) => {
+            const state = buttonState(question.id);
+            return (
+              <AnswerOptionButton key={question.id} kind={isDesktop ? 'prize-desktop' : 'prize-mobile'} onClick={() => {}} state={state}>
+                <div className={clsx(styles['l-question-prize'], state === 'active' && styles['question-prize__title--active'])}>{question.prize}</div>
+              </AnswerOptionButton>
+            );
+          })}
         </div>
       </div>
       <div className={clsx(styles.overlay, isMenuOpen && styles['overlay--active'])} onClick={handleMenuClick} />
