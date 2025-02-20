95feab06a082ac8d3020a26582e2dc7e930a70a4
IvaTsu <ivan.tsud@gmail.com>
2025-02-20 14:42:31 +0100

    feat: reset game

--- src/components/buttons/start-button/start-button.tsx
+++ src/components/buttons/start-button/start-button.tsx
@@ -2,10 +2,18 @@
 
 import { redirect } from 'next/navigation';
 import route from '@/route';
+import useGameStore from '@/store/gameStore';
 import styles from './start-button.module.css';
 
 export default function StartButton({ text }: {text: string}) {
+  const { setGameStart } = useGameStore();
+
+  function handleClick() {
+    setGameStart();
+    redirect(route.game);
+  }
+
   return (
-    <button type="button" className={styles.button} onClick={() => redirect(route.game)}>{text}</button>
+    <button type="button" className={styles.button} onClick={handleClick}>{text}</button>
   );
 }
